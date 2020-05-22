0478659d6ea102e512ec415150fefce46d71dab7
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-22 20:32:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ p programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-	programmer_hash[:yukihiro_matsumoto,{:known_for => "Ruby",:languages => ["LISP", "C"]}]
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}]
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
